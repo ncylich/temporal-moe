165eb180ffdef1daaf07715d445bcac33fb9a729
haok1402 <haok1402@gmail.com>
2025-03-15 18:25:07 -0400

    reorganize the arguments into different compartments

---
+++ configs/infra/llama-182M-1.4B.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# For a comprehensive list of all available arguments, refer to:
+# - https://github.com/NVIDIA/Megatron-LM/blob/main/megatron/training/arguments.py
+
+TORCH_ARGS=(
+    --nnodes $SLURM_NNODES
+    --node_rank $SLURM_NODEID
+    --nproc_per_node $SLURM_GPUS_ON_NODE
+    --rdzv-id $SLURM_JOB_ID
+    --rdzv-backend c10d
+    --rdzv-endpoint $MASTER_ADDR:$MASTER_PORT
+)
+
+INFRA_ARGS=(
+    --tensor-model-parallel-size 1
+    --pipeline-model-parallel-size 1
+    --expert-model-parallel-size 1
+    --sequence-parallel
+    --use-distributed-optimizer
+    --overlap-grad-reduce
+    --overlap-param-gather
+)
---
+++ configs/logging.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# For a comprehensive list of all available arguments, refer to:
+# - https://github.com/NVIDIA/Megatron-LM/blob/main/megatron/training/arguments.py
+
+LOG_ARGS=(
+    --log-interval 10
+    --log-throughput
+    --save $WEIGHTS_PATH
+    --save-interval 10000
+    --load $WEIGHTS_PATH
+    --eval-interval 1000
+    --eval-iters 100
+    --tensorboard-dir "${WEIGHTS_PATH}/tensorboard"
+    --wandb-project "MoE-Research"
+    --wandb-exp-name $PROJECT_NAME
+)
