b940af616900402d7663654bfa7f8090463d2419
haok1402 <haok1402@gmail.com>
2025-03-27 09:32:34 -0400

    a task may involve multiple steps... we've therefore rename things around

--- scripts/dataset/download.sh
+++ scripts/dataset/download.sh
@@ -45,7 +45,7 @@ download_dclm28b() {
         echo $file >> $task
     done
     # Dispatch the tasks to the nodes.
-    srun -W 0 scripts/dataset/functions/download_dclm.sh
+    srun -W 0 scripts/dataset/functions/download_dclm_step1.sh
 }
 
 case $1 in
