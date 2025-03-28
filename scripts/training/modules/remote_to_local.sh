73706ab91bf8d72fc5d2f833a9b39970f666a674
haok1402 <haok1402@gmail.com>
2025-03-28 16:08:06 -0400

    it is a bad idea to delete unmatched destination given that caches exist

--- scripts/training/modules/remote_to_local.sh
+++ scripts/training/modules/remote_to_local.sh
@@ -2,6 +2,6 @@
 # This script is used to synchronize files from Google Cloud Storage to the local filesystem.
 
 if [ $SLURM_LOCALID -eq 0 ]; then
-    gcloud storage rsync --recursive --delete-unmatched-destination-objects $GCP_DATASET_PATH $DATASET_PATH
-    gcloud storage rsync --recursive --delete-unmatched-destination-objects $GCP_WEIGHTS_PATH $WEIGHTS_PATH
+    gcloud storage rsync --recursive $GCP_DATASET_PATH $DATASET_PATH
+    gcloud storage rsync --recursive $GCP_WEIGHTS_PATH $WEIGHTS_PATH
 fi
