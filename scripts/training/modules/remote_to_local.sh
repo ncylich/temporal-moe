ee2b7198cb3a205c7ca16a70e9dd9ac7b9ec0b76
haok1402 <haok1402@gmail.com>
2025-04-05 16:40:26 -0400

    fix the styling of remote_to_local script

--- scripts/training/modules/remote_to_local.sh
+++ scripts/training/modules/remote_to_local.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# This script is used to synchronize files from Google Cloud Storage to the local filesystem.
+# Sync files from GCP to local filesystem
+# Author: Hao Kang | Date: April 5, 2025
 
-if [ $SLURM_LOCALID -eq 0 ]; then
-    gcloud storage rsync --recursive $GCP_DATASET_PATH $DATASET_PATH
-    gcloud storage rsync --recursive $GCP_WEIGHTS_PATH $WEIGHTS_PATH
+# Only run on the first local task
+if [ "$SLURM_LOCALID" -eq 0 ]; then
+    gcloud storage rsync --recursive "$GCP_DATASET_PATH" "$DATASET_PATH"
+    gcloud storage rsync --recursive "$GCP_WEIGHTS_PATH" "$WEIGHTS_PATH"
 fi
