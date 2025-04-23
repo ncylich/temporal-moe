3d0564f2b32bab8aef97fd23c132eb4a21f98433
haok1402 <haok1402@gmail.com>
2025-04-23 13:47:33 -0400

    delete a bunch of things

--- scripts/profiling/modules/download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Only one task per node gets to download.
-if [ "$SLURM_LOCALID" -eq 0 ]; then
-    gcloud storage rsync --recursive "$GCP_DATASET_PATH" "$DATASET_PATH"
-    gcloud storage rsync --recursive "$GCP_PROFILE_PATH" "$PROFILE_PATH"
-fi
