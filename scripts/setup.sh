567d15f43e05935faae00ea9fc67e55343145274
haok1402 <haok1402@gmail.com>
2025-04-23 14:26:26 -0400

    force the training script to be using cuda 12.4

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,6 +18,8 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda create -n MoE python=3.10 -y
 conda activate MoE
 
+pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+
 pip install -r Megatron-LM/requirements/pytorch_24.10/requirements.txt
 
 pushd apex
