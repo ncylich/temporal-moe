901bf12d230ae181bc67f0b8c3fdc7751d02ff4d
haok1402 <haok1402@gmail.com>
2025-04-24 21:17:49 -0400

    add the weights for ablation... in conda

--- scripts/training/flame-moe.sh
+++ scripts/training/flame-moe.sh
@@ -18,6 +18,7 @@ source scripts/secret.sh
 
 export RDZV_BACKEND="c10d"
 export RDZV_ENDPOINT="${RDZV_ENDPOINT:-$(hostname):8000}"
+export TRAIN_WEIGHTS="${TRAIN_WEIGHTS:-$GCP_WEIGHTS/flame-moe/$SLURM_JOB_ID}"
 
 srun -W 0 scripts/training/modules/flame-moe_step1.sh
 srun -W 0 scripts/training/modules/flame-moe_step2.sh
