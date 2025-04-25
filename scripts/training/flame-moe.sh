df923c1b96b8f2f455ba82d5a9cf41347b7466ed
haok1402 <haok1402@gmail.com>
2025-04-24 21:48:15 -0400

    fix the wandb entity and project!

--- scripts/training/flame-moe.sh
+++ scripts/training/flame-moe.sh
@@ -18,6 +18,8 @@ source scripts/secret.sh
 
 export RDZV_BACKEND="c10d"
 export RDZV_ENDPOINT="${RDZV_ENDPOINT:-$(hostname):8000}"
+export WANDB_ENTITY="${WANDB_ENTITY:-haok}"
+export WANDB_PROJECT="${WANDB_PROJECT:-flame-moe}"
 export TRAIN_WEIGHTS="${TRAIN_WEIGHTS:-$GCP_WEIGHTS/flame-moe/$SLURM_JOB_ID}"
 
 srun -W 0 scripts/training/modules/flame-moe_step1.sh
