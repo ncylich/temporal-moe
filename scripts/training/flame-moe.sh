9b6fee0cb5fc8a12c10b96ce69ad8c83b2e7cdbd
haok1402 <haok1402@gmail.com>
2025-04-29 02:14:00 -0400

    choose our custom modification of the Megatraon-LM library

--- scripts/training/flame-moe.sh
+++ scripts/training/flame-moe.sh
@@ -20,6 +20,9 @@ export RDZV_BACKEND="c10d"
 export RDZV_ENDPOINT="${RDZV_ENDPOINT:-$(hostname):8000}"
 export WANDB_ENTITY="${WANDB_ENTITY:-haok}"
 export WANDB_PROJECT="${WANDB_PROJECT:-flame-moe}"
+export WANDB_RUN_GROUP="${WANDB_RUN_GROUP:-pretrain}"
+export WANDB_NAME="${WANDB_PROJECT:-$SLURM_JOB_ID}"
+export TRAIN_DATASET="${TRAIN_DATASET:-$GCP_DATASET/dclm-28b/tokenized/EleutherAI/pythia-12b}"
 export TRAIN_WEIGHTS="${TRAIN_WEIGHTS:-$GCP_WEIGHTS/flame-moe/$SLURM_JOB_ID}"
 
 srun -W 0 scripts/training/modules/flame-moe_step1.sh
