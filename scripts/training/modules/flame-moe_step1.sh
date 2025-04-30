de80b341be84229e195e5534400cb95449a26316
haok1402 <haok1402@gmail.com>
2025-04-30 16:58:55 -0400

    don't be quite at download; we still need to show the progress!

--- scripts/training/modules/flame-moe_step1.sh
+++ scripts/training/modules/flame-moe_step1.sh
@@ -4,7 +4,7 @@
 i=1; host=$(hostname)
 mkdir -p $SSD_DATASET
 
-until gsutil -q -m rsync -r $TRAIN_DATASET $SSD_DATASET; do
+until gsutil -m rsync -r $TRAIN_DATASET $SSD_DATASET; do
     echo "[$host] Retry attempt $i"
     delay=$((2**i))
     [ $delay -gt 32 ] && delay=32
