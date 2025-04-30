342dc557e8c9e8575ab4db3e22605078fb95f32c
haok1402 <haok1402@gmail.com>
2025-04-30 01:30:19 -0400

    be a bit more fault tolerant in the checkpoint upload procedures

--- scripts/training/modules/flame-moe_step2.sh
+++ scripts/training/modules/flame-moe_step2.sh
@@ -25,17 +25,33 @@ SAVE_ARGS=(
     --tensorboard-dir $SSD_WEIGHTS
 )
 
+host=$(hostname)
 mkdir -p $SSD_WEIGHTS
+
 cd Megatron-LM && torchrun "${TORCH_ARGS[@]}" pretrain_gpt.py \
     "${MODEL_ARGS[@]}" "${INFRA_ARGS[@]}" "${TRAIN_ARGS[@]}" "${DATA_ARGS[@]}" "${SAVE_ARGS[@]}" &
 
+# Start a background upload loop while training runs
 TORCHRUN_PID=$!
 (
     while kill -0 $TORCHRUN_PID 2>/dev/null; do
-        sleep 30m
-        gsutil -m rsync -r $SSD_WEIGHTS $TRAIN_WEIGHTS
+        sleep 15m; i=1
+        until gsutil -q -m rsync -r "$SSD_WEIGHTS" "$TRAIN_WEIGHTS"; do
+            echo "[$host] Upload retry attempt $i"
+            delay=$((2**i))
+            [ $delay -gt 32 ] && delay=32
+            sleep $delay
+            i=$((i + 1))
+        done
     done
 ) &
 
-wait $TORCHRUN_PID
-gsutil -m rsync -r $SSD_WEIGHTS $TRAIN_WEIGHTS
+# Final upload after training finishes
+wait $TORCHRUN_PID; i=1
+until gsutil -m rsync -r "$SSD_WEIGHTS" "$TRAIN_WEIGHTS"; do
+    echo "[$host] Final upload retry attempt $i"
+    delay=$((2**i))
+    [ $delay -gt 32 ] && delay=32
+    sleep $delay
+    i=$((i + 1))
+done
