a9dff5bff26f5a87cebd8eb81d2163c3b4f877fe
haok1402 <haok1402@gmail.com>
2025-05-01 10:57:25 -0400

    revise the script to delete any leftover storage

--- scripts/training/modules/flame-moe_step1.sh
+++ scripts/training/modules/flame-moe_step1.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Delete leftover storage from my previous jobs
+find /mnt/localssd -mindepth 1 -maxdepth 1 -type d -user $USER \
+    ! -name docker ! -name job_tmp ! -name lost+found ! \
+    -name "slurm-$SLURM_JOB_ID" -exec rm -rvf {} \;
+
+# Download the training dataset (retry until successful)
 mkdir -p $SSD_DATASET
 until gcloud storage cp --recursive $TRAIN_DATASET/ $SSD_DATASET/; do continue; done
