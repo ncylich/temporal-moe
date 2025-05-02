e92a3af1a80bcf2849934e6d3767bec564ad12c7
haok1402 <haok1402@gmail.com>
2025-05-02 13:49:46 -0400

    add another set of validation suit to train for

--- scripts/ablation/testing.sh
+++ scripts/ablation/testing.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Optimal, according to the scaling law, is around 2.4B.
-
 # 2.5B Active Parameters
 export NUM_LAYERS=27
 export HIDDEN_SIZE=2048
@@ -23,6 +21,18 @@ export MOE_LAYER_FREQ=[0]*1+[1]*23
 export MICRO_BATCH_SIZE=2
 export EXPERT_MODEL_PARALLEL_SIZE=8
 export TRAIN_ITERS=8495
+export WANDB_RUN_GROUP=testing-2.4e20
+sbatch --nodes=8 scripts/training/flame-moe.sh
+
+# 1.7B Active Parameters
+export NUM_LAYERS=18
+export HIDDEN_SIZE=2048
+export FFN_HIDDEN_SIZE=10944
+export MOE_FFN_HIDDEN_SIZE=1408
+export MOE_LAYER_FREQ="[0]*1+[1]*17"
+export MICRO_BATCH_SIZE=4
+export EXPERT_MODEL_PARALLEL_SIZE=8
+export TRAIN_ITERS=18857
 export WANDB_RUN_GROUP=testing-2.4e20
 sbatch --nodes=8 scripts/training/flame-moe.sh
 
