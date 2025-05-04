900a2db0e8252b491cd09bc48608507bbc18dc5f
haok1402 <haok1402@gmail.com>
2025-05-04 14:49:39 -0400

    let's use docker to forcibly removed all the hanging directories

--- scripts/training/modules/flame-moe_step1.sh
+++ scripts/training/modules/flame-moe_step1.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Remove leftover directories from previous jobs
-find /mnt/localssd -mindepth 1 -maxdepth 1 -type d -user $USER \
-    ! -name docker ! -name job_tmp ! -name lost+found ! -exec rm -rvf {} \;
+docker run --rm -it -v /mnt/localssd:/mnt/localssd ubuntu bash -c "find /mnt/localssd -mindepth 1 -maxdepth 1 ! -name docker ! -name job_tmp -exec rm -rvf {} +"
 
 # Download training dataset (retry until success)
-mkdir -p $SSD_DATASET
 echo "[$(hostname)] Fetching $TRAIN_DATASET ..."
+mkdir -p $SSD_DATASET
 until gcloud storage cp --quiet --recursive $TRAIN_DATASET/ $SSD_DATASET/; do continue; done
 echo "[$(hostname)] Done."
