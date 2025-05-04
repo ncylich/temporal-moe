df3a925e4f2c855b4f5214b1f737084ff1ccccd4
haok1402 <haok1402@gmail.com>
2025-05-04 15:15:53 -0400

    remove interactive from directories cleanup

--- scripts/training/modules/flame-moe_step1.sh
+++ scripts/training/modules/flame-moe_step1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove leftover directories from previous jobs
-docker run --rm -it -v /mnt/localssd:/mnt/localssd ubuntu bash -c "find /mnt/localssd -mindepth 1 -maxdepth 1 ! -name docker ! -name job_tmp -exec rm -rvf {} +"
+docker run --rm -v /mnt/localssd:/mnt/localssd ubuntu bash -c "find /mnt/localssd -mindepth 1 -maxdepth 1 ! -name docker ! -name job_tmp -exec rm -rvf {} +"
 
 # Download training dataset (retry until success)
 echo "[$(hostname)] Fetching $TRAIN_DATASET ..."
