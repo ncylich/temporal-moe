6095195dc9452017bf1c35eb64f17377839c08e3
haok1402 <haok1402@gmail.com>
2025-05-05 17:11:53 -0400

    fix the code for 1.7b too

---
+++ scripts/release/flame-moe-1.7b.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export NUM_LAYERS=18
+export HIDDEN_SIZE=2048
+export FFN_HIDDEN_SIZE=10944
+export MOE_FFN_HIDDEN_SIZE=1408
+export MOE_LAYER_FREQ="[0]*1+[1]*17"
+export MICRO_BATCH_SIZE=8
+export PIPELINE_MODEL_PARALLEL_SIZE=8
+export EXPERT_MODEL_PARALLEL_SIZE=8
+export TRAIN_ITERS=11029
+export SAVE_INTERVAL=1000
+export EVAL_INTERVAL=1000
+sbatch --job-name=flame-moe-1.7b --nodes=8 scripts/training/flame-moe.sh
