15c9f56dcdcc0b736e38a4264eb41ed1162f4fc9
haok1402 <haok1402@gmail.com>
2025-05-05 13:17:55 -0400

    let's save and evaluate on a per-1k basis

--- scripts/release/flame-moe-4.3b.sh
+++ scripts/release/flame-moe-4.3b.sh
@@ -5,8 +5,10 @@ export HIDDEN_SIZE=2048
 export FFN_HIDDEN_SIZE=10944
 export MOE_FFN_HIDDEN_SIZE=1408
 export MOE_LAYER_FREQ="[0]*1+[1]*47"
-export MICRO_BATCH_SIZE=1
+export MICRO_BATCH_SIZE=4
 export PIPELINE_MODEL_PARALLEL_SIZE=4
 export EXPERT_MODEL_PARALLEL_SIZE=16
 export TRAIN_ITERS=17337
+export SAVE_INTERVAL=1000
+export EVAL_INTERVAL=1000
 sbatch --job-name=flame-moe-4.3b --nodes=8 scripts/training/flame-moe.sh
