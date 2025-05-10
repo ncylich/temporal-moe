1ef60c7d75b6ee7a6fa555ce342a5993cc8d7121
haok1402 <haok1402@gmail.com>
2025-05-10 15:01:17 -0400

    revise the scripts for calculating expert specialization;

--- empirical_analysis/expert_specialization.sh
+++ empirical_analysis/expert_specialization.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-for checkpoint_step in {1100,2200,3300,4400}; do
+for checkpoint_step in {11029,9900,8800}; do
+    for layer_number in {18,}; do
+        for expert_index in {0..64}; do
+            timeout 10m python3 empirical_analysis/work/expert_specialization.py \
+                --layer-number $layer_number --expert-index $expert_index --checkpoint-step $checkpoint_step
+        done
+    done
+done
+
+for checkpoint_step in {1100,2200,4400}; do
     for layer_number in {18,}; do
         for expert_index in {0..64}; do
             timeout 10m python3 empirical_analysis/work/expert_specialization.py \
@@ -10,15 +19,6 @@ for checkpoint_step in {1100,2200,3300,4400}; do
 done
 
 for checkpoint_step in {5500,6600,7700}; do
-    for layer_number in {18,}; do
-        for expert_index in {0..64}; do
-            timeout 10m python3 empirical_analysis/work/expert_specialization.py \
-                --layer-number $layer_number --expert-index $expert_index --checkpoint-step $checkpoint_step
-        done
-    done
-done
-
-for checkpoint_step in {8800,9900,11029}; do
     for layer_number in {18,}; do
         for expert_index in {0..64}; do
             timeout 10m python3 empirical_analysis/work/expert_specialization.py \
