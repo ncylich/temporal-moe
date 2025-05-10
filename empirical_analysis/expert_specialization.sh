306aff3fcc6c2f3cab78088e53bfe5f2f722980b
haok1402 <haok1402@gmail.com>
2025-05-10 15:16:35 -0400

    fix the code... bash does include the last index

--- empirical_analysis/expert_specialization.sh
+++ empirical_analysis/expert_specialization.sh
@@ -2,7 +2,7 @@
 
 for checkpoint_step in {11029,9900,8800}; do
     for layer_number in {18,}; do
-        for expert_index in {0..64}; do
+        for expert_index in {0..63}; do
             timeout 10m python3 empirical_analysis/work/expert_specialization.py \
                 --layer-number $layer_number --expert-index $expert_index --checkpoint-step $checkpoint_step
         done
@@ -11,7 +11,7 @@ done
 
 for checkpoint_step in {1100,2200,4400}; do
     for layer_number in {18,}; do
-        for expert_index in {0..64}; do
+        for expert_index in {0..63}; do
             timeout 10m python3 empirical_analysis/work/expert_specialization.py \
                 --layer-number $layer_number --expert-index $expert_index --checkpoint-step $checkpoint_step
         done
@@ -20,7 +20,7 @@ done
 
 for checkpoint_step in {5500,6600,7700}; do
     for layer_number in {18,}; do
-        for expert_index in {0..64}; do
+        for expert_index in {0..63}; do
             timeout 10m python3 empirical_analysis/work/expert_specialization.py \
                 --layer-number $layer_number --expert-index $expert_index --checkpoint-step $checkpoint_step
         done
