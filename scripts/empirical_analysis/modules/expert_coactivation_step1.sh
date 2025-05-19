a5c6e5dabef71f1d77c2be7bf696f2eb62013e3a
haok1402 <haok1402@gmail.com>
2025-05-19 17:36:59 -0400

    revise the download script, it's much faster for smaller files to use one process directly

--- scripts/empirical_analysis/modules/expert_coactivation_step1.sh
+++ scripts/empirical_analysis/modules/expert_coactivation_step1.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 mkdir -p $SSD_MOUNT/actives
-gcloud storage ls --recursive $GCP_MOUNT/actives/$TRAIN_JOB_NAME/$TRAIN_JOB_ID/ | grep -v '^$' | grep -v ':$' | awk '{print $NF}' | xargs -P 128 -I {} bash -c '
-        dest="$SSD_MOUNT/actives/$(echo {} | sed "s|$GCP_MOUNT/actives/$TRAIN_JOB_NAME/$TRAIN_JOB_ID/||")"
-        mkdir -p "$(dirname "$dest")"
-        until gcloud storage cp --no-user-output-enabled "{}" "$dest"; do continue; done
-    '
+gcloud storage cp --recursive $GCP_MOUNT/actives/$TRAIN_JOB_NAME/$TRAIN_JOB_ID/* $SSD_MOUNT/actives > /dev/null 2>&1
