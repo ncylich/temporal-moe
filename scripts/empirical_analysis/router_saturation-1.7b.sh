7ae5ddc5138840f957ddc7c1dbf6cfc17a256099
haok1402 <haok1402@gmail.com>
2025-05-20 14:59:19 -0400

    significantly speedup the code for measuring router saturation

--- scripts/empirical_analysis/router_saturation-1.7b.sh
+++ scripts/empirical_analysis/router_saturation-1.7b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Compute the router saturation for FLAME-MoE-290M-1.3B
+# Compute the router saturation for FLAME-MoE-1.7B-10.3B
 
-#SBATCH --job-name=router-saturation-290m
+#SBATCH --job-name=router-saturation-1.7b
 #SBATCH --output=logs/%x/%j.log
 
 #SBATCH --partition=flame
@@ -17,15 +17,15 @@
 source scripts/config.sh
 
 # load the captured actives via google cloud
-export TRAIN_JOB_ID=31066
-export TRAIN_JOB_NAME=flame-moe-290m
+export TRAIN_JOB_ID=31245
+export TRAIN_JOB_NAME=flame-moe-1.7b
 bash scripts/empirical_analysis/modules/router_saturation_step1.sh
 
-# process each top-k for FLAME-MoE-290M-1.3B
+# process each top-k for FLAME-MoE-1.7B-10.3B
 for moe_router_topk in 1 2 4 6; do
-    for layer_number in {2..9}; do
+    for layer_number in {2..18}; do
         actives_pattern="$SSD_MOUNT/actives/*/$layer_number"
-        results_path=results/router-saturation/flame-moe-290m/$moe_router_topk.pkl
+        results_path=results/router-saturation/flame-moe-1.7b/$moe_router_topk.pkl
         python3 scripts/empirical_analysis/modules/router_saturation_step2.py --moe-router-topk $moe_router_topk --actives-pattern "$actives_pattern" --results-path $results_path
     done
 done
